0b96dd7dcefeb3c2349af252ebfd22b80ae1716c
demattei-teo <teitodematteio@gmail.com>
2024-03-20 20:33:29 -0300

    feat: add sub menus to navbar

--- src/utils/Links.ts
+++ src/utils/Links.ts
@@ -1,63 +1,63 @@
 const categories = {
   archivo: [
-    { name: 'ABM Sede', link: '/' },
-    { name: 'ABM Campaña', link: '/' },
-    { name: 'ABM Cohorte', link: '/' },
-    { name: 'ABM Modalidad', link: '/' },
-    { name: 'ABM Nivel Estudio', link: '/' },
-    { name: 'ABM Elección ISSD', link: '/' },
-    { name: 'ABM Elección Carrera', link: '/' },
-    { name: 'ABM Carga Semanal', link: '/' },
-    { name: 'ABM Tipo DNI', link: '/' },
-    { name: 'ABM Sexo', link: '/' },
-    { name: 'ABM Estado Civil', link: '/' },
-    { name: 'ABM Tipo Vivienda', link: '/' },
-    { name: 'ABM Parentesco', link: '/' },
-    { name: 'ABM Ocupación', link: '/' },
-    { name: 'ABM Vive Con', link: '/' },
-    { name: 'ABM Pais', link: '/' },
-    { name: 'ABM Provincia', link: '/' },
-    { name: 'ABM Localidad', link: '/' },
-    { name: 'ABM Barrio', link: '/' }
+    { name: 'ABM Sede', link: '/', subMenu: [{ name: 'Consultar Sede' }] },
+    { name: 'ABM Campaña', link: '/', subMenu: [{ name: 'Consultar Campaña' }] },
+    { name: 'ABM Cohorte', link: '/', subMenu: [{ name: 'Consultar Cohorte' }] },
+    { name: 'ABM Modalidad', link: '/', subMenu: [{ name: 'Consultar Modalidad' }] },
+    { name: 'ABM Nivel Estudio', link: '/', subMenu: [{ name: 'Consultar Nivel Estudio' }] },
+    { name: 'ABM Elección ISSD', link: '/', subMenu: [{ name: 'Consultar Elección ISSD' }] },
+    { name: 'ABM Elección Carrera', link: '/', subMenu: [{ name: 'Consultar Elección Carrera' }] },
+    { name: 'ABM Carga Semanal', link: '/', subMenu: [{ name: 'Consultar Carga Semanal' }] },
+    { name: 'ABM Tipo DNI', link: '/', subMenu: [{ name: 'Consultar Tipo DNI' }] },
+    { name: 'ABM Sexo', link: '/', subMenu: [{ name: 'Consultar Sexo' }] },
+    { name: 'ABM Estado Civil', link: '/', subMenu: [{ name: 'Consultar Estado Civil' }] },
+    { name: 'ABM Tipo Vivienda', link: '/', subMenu: [{ name: 'Consultar Tipo Vivienda' }] },
+    { name: 'ABM Parentesco', link: '/', subMenu: [{ name: 'Consultar Parentesco' }] },
+    { name: 'ABM Ocupación', link: '/', subMenu: [{ name: 'Consultar Ocupación' }] },
+    { name: 'ABM Vive Con', link: '/', subMenu: [{ name: 'Consultar Vive Con' }] },
+    { name: 'ABM Pais', link: '/', subMenu: [{ name: 'Consultar Pais' }] },
+    { name: 'ABM Provincia', link: '/', subMenu: [{ name: 'Consultar Provincia' }] },
+    { name: 'ABM Localidad', link: '/', subMenu: [{ name: 'Consultar Localidad' }] },
+    { name: 'ABM Barrio', link: '/', subMenu: [{ name: 'Consultar Barrio' }] }
   ],
   carreras: [
-    { name: 'ABM Carreras', link: '/' },
-    { name: 'ABM Materias', link: '/' },
-    { name: 'ABM Planes Estudio', link: '/' },
-    { name: 'ABM Correlativas', link: '/' },
-    { name: 'ABM Material Didáctico', link: '/' }
+    { name: 'ABM Carreras', link: '/', subMenu: [{ name: 'Consultar Carreras' }] },
+    { name: 'ABM Materias', link: '/', subMenu: [{ name: 'Consultar Materias' }] },
+    { name: 'ABM Planes Estudio', link: '/', subMenu: [{ name: 'Consultar Planes Estudio' }] },
+    { name: 'ABM Correlativas', link: '/', subMenu: [{ name: 'Consultar Correlativas' }] },
+    { name: 'ABM Material Didáctico', link: '/', subMenu: [{ name: 'Consultar Material Didáctico' }] }
   ],
   alumnos: [
-    { name: 'ABM Alumnos', link: '/' },
+    { name: 'ABM Alumnos', link: '/', subMenu: [{ name: 'Consultar Alumnos' }] },
     { name: 'Historico Alumnos', link: '/' },
-    { name: 'ABM Equivalencias', link: '/' },
-    { name: 'ABM Asistencias', link: '/' },
-    { name: 'ABM Pases', link: '/' }
+    { name: 'ABM Equivalencias', link: '/', subMenu: [{ name: 'Consultar Equivalencias' }] },
+    { name: 'ABM Asistencias', link: '/', subMenu: [{ name: 'Consultar Asistencias' }] },
+    { name: 'ABM Pases', link: '/', subMenu: [{ name: 'Consultar Pases' }] }
   ],
   docentes: [
-    { name: 'ABM Docentes', link: '/' },
+    { name: 'ABM Docentes', link: '/', subMenu: [{ name: 'Consultar Docentes' }] },
     { name: 'Histórico Docentes', link: '/' },
     { name: 'Emitir Certificado', link: '/' },
-    { name: 'ABM Tardanzas', link: '/' },
-    { name: 'Inasistencias', link: '/' },
-    { name: 'ABM Marcas', link: '/' }
+    { name: 'ABM Tardanzas', link: '/', subMenu: [{ name: 'Consultar Tardanzas' }] },
+    { name: 'ABM Inasistencias', link: '/', subMenu: [{ name: 'Consultar Inasistencias' }] },
+    { name: 'ABM Marcas', link: '/', subMenu: [{ name: 'Consultar Marcas' }] }
   ],
   cursado: [
-    { name: 'ABM Inscripciónes Cursado', link: '/' },
-    { name: 'ABM Inscripción Examen', link: '/' },
-    { name: 'ABM Turno Examen', link: '/' },
-    { name: 'ABM Programación Horarios', link: '/' }
+    { name: 'ABM Inscripciónes Cursado', link: '/', subMenu: [{ name: 'Consultar Cursado' }] },
+    { name: 'ABM Inscripción Examen', link: '/', subMenu: [{ name: 'Consultar Examen' }] },
+    { name: 'ABM Turno Examen', link: '/', subMenu: [{ name: 'Consultar Turno' }] },
+    { name: 'ABM Programación Horarios', link: '/', subMenu: [{ name: 'Consultar Programación Horarios' }] }
   ],
   campañas: [
-    { name: 'ABM Asesores', link: '/' },
-    { name: 'ABM Liquidación Asedores', link: '/' },
-    { name: 'ABM Convocatorias', link: '/' }
+    { name: 'ABM Asesores', link: '/', subMenu: [{ name: 'Consultar Asesores' }] },
+    { name: 'ABM Liquidación Asesores', link: '/', subMenu: [{ name: 'Consultar Liquidación Asesores' }] },
+    { name: 'ABM Convocatorias', link: '/', subMenu: [{ name: 'Consultar Convocatorias' }] }
   ],
   administración: [
-    { name: 'ABM Pagos', link: '/' },
-    { name: 'ABM Recibos', link: '/' },
-    { name: 'ABM Compromisos Pago', link: '/' },
-    { name: 'ABM Plan de Cuotas', link: '/' }
+    { name: 'ABM Pagos', link: '/', subMenu: [{ name: 'Consultar Pagos' }] },
+    { name: 'ABM Recibos', link: '/', subMenu: [{ name: 'Consultar Recibos' }] },
+    { name: 'ABM Compromisos Pago', link: '/', subMenu: [{ name: 'Consultar Compromisos' }] },
+    { name: 'ABM Plan de Cuotas', link: '/', subMenu: [{ name: 'Consultar Plan de Cuotas' }] }
   ]
 }
 
